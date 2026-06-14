37273d42b4f706b7f042c8a016ec25b4bab2124b
RecursiveOm <omkar.zunje123@gmail.com>
2026-06-14 09:46:35 +0530

    Initialize Audion backend architecture
    - Setup Express server foundation
    - Configure MongoDB database connection
    - Add environment variable support
    - Create role based user schema
    - Establish scalable backend project structure

---
+++ src/db/db.js
@@ -0,0 +1,18 @@
+    const mongoose = require("mongoose")
+
+    async function connectDB(){
+        try{
+            await mongoose.connect(process.env.MONGODB_CONNECT_URL)
+            console.log("Connected to DB");
+            
+
+        }catch(err){
+
+            console.log("Error connecting to database");
+
+
+
+        }
+    }
+
+    module.exports = connectDB;
