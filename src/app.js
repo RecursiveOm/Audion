ef90e68047ad1501a40e1f47db0cb5be9b363f4b
RecursiveOm <omkar.zunje123@gmail.com>
2026-06-14 23:20:19 +0530

    Implement artist music upload pipeline
    - Add music upload controller and routes
    - Create music schema with artist references
    - Integrate ImageKit cloud storage service
    - Implement role based artist upload authorization
    - Store uploaded music metadata in MongoDB
    - Add multipart file upload support

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const authRoutes  = require("./routes/auth.routes")
-
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes  = require("./routes/auth.routes");
+const musicRoutes = require('./routes/music.routes');
 
 
 const app = express()
@@ -10,5 +10,5 @@ app.use(cookieParser());
 
 
 app.use('/api/auth', authRoutes);
-
+app.use('/api/music',musicRoutes);
 module.exports = app
---
+++ src/controllers/music.controller.js
@@ -0,0 +1,94 @@
+const musicModel = require("../models/music.model")
+const jwt = require("jsonwebtoken")
+const uploadFile = require("../services/storage.service")
+
+
+async function createMusic(req,res){
+
+    const token = req.cookies.token
+
+
+    if(!token){
+
+        return res.status(401).json({
+
+            message:"Unauthorized user"
+
+        })
+
+    }
+
+
+    try{
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        )
+
+
+        if(decoded.role !== "artist"){
+
+            return res.status(403).json({
+
+                message:"You dont have access to create music"
+
+            })
+
+        }
+
+
+        const { title } = req.body
+
+        const file = req.file
+
+
+        const result = await uploadFile(file.buffer)
+
+
+        const music = await musicModel.create({
+
+            uri:result.url,
+
+            title,
+
+            artist:decoded.id
+
+        })
+
+
+        res.status(201).json({
+
+            message:"Music Created Successfully",
+
+            music:{
+
+                id:music._id,
+
+                uri:music.uri,
+
+                title:music.title,
+
+                artist:music.artist
+
+            }
+
+        })
+
+
+    }catch(err){
+
+    console.log("REAL ERROR:", err)
+
+    return res.status(500).json({
+
+        message:err.message
+
+    })
+
+}
+
+}
+
+
+module.exports = { createMusic }
